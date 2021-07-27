3e2eafbd516c14114120446bbb30163befd3cbda
Raul Franco <raul.franco@norvoz.es>
2021-07-27 13:59:46 +0200

    archivo deps.js

---
+++ deps.js
@@ -0,0 +1,3 @@
+export * from  "https://deno.land/x/dotenv/load.ts";
+export { Client } from "https://deno.land/x/mysql/mod.ts";
+export { Application, Router } from "https://deno.land/x/oak/mod.ts";
